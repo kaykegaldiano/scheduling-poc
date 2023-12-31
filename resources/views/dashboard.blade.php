5c1ae2b8d529e058dba15181fb2ce97473d7ef46
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-12-31 15:06:59 -0300

    feat: update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Agendamento') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
                     <form action="" method="POST">
                         @csrf
 
-                        <x-input-label for="date" value="Select date" />
-                        <x-text-input id="date" name="date" />
+                        <div class="flex flex-col gap-1">
+                            <x-input-label for="schedule_date" value="Selecione a data" />
+                            <x-text-input id="schedule_date" name="schedule_date" />
+                        </div>
+
+                        <x-primary-button class="mt-4">Agendar</x-primary-button>
 
                     </form>
 
@@ -22,4 +26,23 @@
             </div>
         </div>
     </div>
+
+    @push('pickr')
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                flatpickr('#schedule_date', {
+                    disable: [
+                        {!! $holidays->count() > 0 ? "'" . implode("', '", $holidays->pluck('date')->all()) . "'" : '' !!},
+                        function(date) {
+                            return (date.getDay() === 0);
+                        }
+                    ],
+                    dateFormat: 'd/m/Y',
+                    minDate: 'today',
+                });
+            })
+        </script>
+    @endpush
+
 </x-app-layout>
+
