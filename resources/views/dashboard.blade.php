93f5464730874e0f10e7541ad3aa5f4c4a8404a0
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-01-07 02:05:06 -0300

    refactor: refactoring js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,13 +22,13 @@
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                    <form action="{{ route('dashboard') }}" method="POST" x-data="{ visible: false }">
+                    <form action="{{ route('dashboard') }}" method="POST" x-data="visible">
                         @csrf
 
                         <div class="flex flex-col gap-1">
                             <x-input-label for="schedule_date" value="Selecione a data" />
                             <x-text-input
-                                @change="$el.value !== '' ? visible = true : visible = false"
+                                @change="await showSchedules"
                                 id="schedule_date"
                                 name="schedule_date"
                             />
@@ -37,7 +37,7 @@
 
                         <div class="flex flex-col gap-1 pt-4" x-show="visible" x-transition>
                             <x-input-label for="schedule_time" value="Selecione o horário" />
-                            <select class="border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" name="schedule_hour" id="schedule_hour">
+                            <select x-ref="schedule_hour" class="border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" name="schedule_hour" id="schedule_hour">
                                 <option value="" selected>-</option>
                             </select>
                             <x-input-error :messages="$errors->get('schedule_hour')" />
@@ -69,40 +69,45 @@
                 });
             })
 
-            document.querySelector('#schedule_date').addEventListener('change', async evt => {
-                if (evt.target.value !== '') {
-                    const schedulesArray = []
-                    const schedules = await fetch('/api/schedules')
-                    const schedulesData = await schedules.json()
+            document.addEventListener('alpine:init', () => {
+                Alpine.data('visible', () => ({
+                    visible: false,
 
-                    const scheduleTimes = await fetch('/api/schedule-times')
-                    const scheduleTimesData = await scheduleTimes.json()
+                    async showSchedules() {
+                        this.$el.value !== '' ? this.visible = true : this.visible = false
 
-                    schedulesData.forEach(schedule => {
-                        scheduleTimesData.forEach(scheduleTimeData => {
-                            if (scheduleTimeData.schedule_date === evt.target.value && scheduleTimeData.schedule_hour === schedule.time) {
-                                schedulesArray.push(schedule.time)
-                            }
+                        const schedulesArray = []
+                        const schedules = await fetch('/api/schedules')
+                        const schedulesData = await schedules.json()
+                        const scheduleTimes = await fetch('/api/schedule-times')
+                        const scheduleTimesData = await scheduleTimes.json()
+
+                        schedulesData.forEach(schedule => {
+                            scheduleTimesData.forEach(scheduleTimeData => {
+                                if (scheduleTimeData.schedule_date === this.$el.value && scheduleTimeData.schedule_hour === schedule.time) {
+                                    schedulesArray.push(schedule.time)
+                                }
+                            })
                         })
-                    })
 
-                    const sel = document.querySelector('#schedule_hour')
+                        const sel = this.$refs.schedule_hour
 
-                    while (sel.options.length > 1) {
-                        sel.remove(1)
-                    }
-
-                    schedulesData.forEach(schedule => {
-                        if (schedulesArray.includes(schedule.time)) {
-                            return
+                        while (sel.options.length > 1) {
+                            sel.remove(1)
                         }
 
-                        const opt = document.createElement('option')
-                        opt.value = schedule.time
-                        opt.text = schedule.time
-                        sel.add(opt, null)
-                    })
-                }
+                        schedulesData.forEach(schedule => {
+                            if (schedulesArray.includes(schedule.time)) {
+                                return
+                            }
+
+                            const opt = document.createElement('option')
+                            opt.value = schedule.time
+                            opt.text = schedule.time
+                            sel.add(opt, null)
+                        })
+                    }
+                }))
             })
         </script>
     @endpush
